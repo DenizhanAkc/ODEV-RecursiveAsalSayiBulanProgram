54f887015ea245f89cfc2b5cda6bc19d088784f9
Denizhan AKCABEL <168652539+DenizhanAkc@users.noreply.github.com>
2024-09-04 22:31:25 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+    public class Test {
+
+        public static boolean asalMi(int sayi, int bolen) {
+            if (sayi == 1) {
+                return false;
+            }
+            if (bolen == 1) {
+                return true;
+            }
+            if (sayi % bolen == 0) {
+                return false;
+            }
+            return asalMi(sayi, bolen-1);
+                }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Bir sayı girin: ");
+            int sayi = scanner.nextInt();
+
+            if (asalMi(sayi, sayi/2)){
+                System.out.println(sayi+ " Asal Sayidir.");
+            }else{
+                System.out.println(sayi+ " Asal Sayi DEGILDIR.");
+            }
+
+
+
+            }
+        }
+
+
+
+
+
+
